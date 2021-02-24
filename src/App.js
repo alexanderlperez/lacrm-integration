b4b92dc6ad29c74848d213b17811dc86d1a64a26
Alex Perez <alexanderlperez@gmail.com>
2021-02-23 17:29:10 -0700

    Working post to lacrm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import styled from '@emotion/styled' 
+import { useForm } from 'react-hook-form'
+
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import firebaseConfig from './firebaseConfig.js'
+import FirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+
+firebase.initializeApp(firebaseConfig);
+
+const LACRMForm = styled('form')`
+  & > * {
+      display: block;
+  }
+`
+
+const NoteForm = () => {
+  const {register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data)
+
+  return (
+    <LACRMForm onSubmit={handleSubmit(onSubmit)}>
+      <select id="team-members" name="team-members" ref={register}>
+        <option value="">asdfasdfasdfasdf</option>
+      </select>
+      <textarea id="name" name="note" ref={register}></textarea>
+      <button type="submit" ref={register}>Submit</button>
+    </LACRMForm>
+  )
+}
+
+const Login = () => {
+  const uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ]
+  }
+
+  return (
+    <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+  )
+}
+
+const Complete = () => {}
 
 export const App = () => {
-    return (
-        <div> Test</div>
-    )
+  const [route, setRoute] = useState('login')
+
+  const routes = {
+    form: <NoteForm />,
+    login: <Login />,
+    complete: <Complete />,
+  }
+
+  return routes[route]
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import { render } from "react-dom";
 
 import { App } from './App'
 
-console.log(App)
+render(<App />, document.getElementById('root'))
 
