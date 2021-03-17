48340bf11447e8214f601d29390ed6a823c5e760
Alex Perez <alexanderlperez@gmail.com>
2021-03-17 15:22:48 -0600

    Latest version, ready to deploy

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,24 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin')
 admin.initializeApp()
 
+const express = require('express');
+const cookieParser = require('cookie-parser')();
+const bodyParser = require('body-parser');
+const cors = require('cors')({origin: true});
+const app = express();
+
+const { format } = require('date-fns')
+
+const API_URL = 'https://api.lessannoyingcrm.com/'
+
+const USER_CODE = '95E24'
+const API_KEY = '613924-AgfdG3fCxn2wJpsHJNIbmQqMGBMkKEp2OYZSAISsRG3zvxwj0X' 
+
+/* Local test data
 const USER_CODE = 'A0DC5'
 const API_KEY = '658885-AcIrvp5jbzw7qhfGvWGzfbZqEPZCmkHzUrc5G2fBOwVXnrJMSE' 
-const API_URL = 'https://api.lessannoyingcrm.com/'
 const DUMMY_CONTACT_ID = '3721904669969614100048897386911' // Test Contact
+*/
 
 const callAPI = async (func, params) => {
   let body = {
@@ -16,18 +30,38 @@ const callAPI = async (func, params) => {
   }
 
   const out = API_URL + '?' + new URLSearchParams(body).toString() + '&Parameters=' + JSON.stringify(params)
-  superagent.post(out).then(res => console.log(res.body))
-  return
-
-
-  return res.text()
+  return superagent.post(out).then(res => res.body)
 }
 
-exports.test = functions.https.onRequest(async (req, res) => {
+app.use(cors);
+app.use(bodyParser.json());
+app.get('/', async (req, res) => {
+  const emails = [
+    'laperez@cfl.rr.com'
+  ]
+
+  console.log(req.query)
+
+  if (!emails.find(e => e === req.query.email)) {
+    console.log(emails, req.query.email)
+    console.log('could not find email!')
+    res.json({Success: false})
+    return 
+  }
+
+  console.log('found email!')
+
+  const { email, note, churchMember } = req.query
+
   const out = await callAPI('CreateNote', {
-    'ContactId': DUMMY_CONTACT_ID,
-    'Note': `Testing ${new Date().toString()}`
+    ContactId: churchMember,
+    Note: 
+    `From ${email} on ${format(new Date(), 'L/d/yyyy h:mmaaa')}:
+${note}
+`
   })
 
-  res.json({result: out})
-})
+  res.json(out)
+});
+
+exports.createNote = functions.https.onRequest(app)
--- src/App.js
+++ src/App.js
@@ -1,59 +1,103 @@
 import React, {useState, useEffect} from 'react'
 import styled from '@emotion/styled' 
 import { useForm } from 'react-hook-form'
+import superagent from 'superagent'
 
+// import firebase from 'firebase/app'
+// import 'firebase/auth'
+// import firebaseConfig from './firebaseConfig.js'
+// import FirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+// 
+// firebase.initializeApp(firebaseConfig);
 
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import firebaseConfig from './firebaseConfig.js'
-import FirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
-
-firebase.initializeApp(firebaseConfig);
+const CF_URL = "https://us-central1-lacrm-integration.cloudfunctions.net/createNote"
+//const CF_URL = "http://127.0.0.1:5001/lacrm-integration/us-central1/createNote"
 
 const LACRMForm = styled('form')`
   & > * {
       display: block;
   }
+
+  textarea, input, select {
+    margin-left: 1em;
+  }
 `
 
 const NoteForm = ({setRoute}) => {
   const {register, handleSubmit, errors } = useForm();
-  const onSubmit = data => setRoute('login')
+  const [hasError, setHasError] = useState(false)
+
+  const onSubmit = data => {
+    const out = superagent
+      .get(CF_URL)
+      .query({ 
+        ...data,
+      })
+      .catch(() => {
+        setHasError(true)
+      })
+      .then(res => {
+        const { Success: success } = JSON.parse(res.text)
+        console.log(res, success)
+
+        if (!success) {
+          setHasError(true)
+          return 
+        }
+
+        setHasError(false)
+        setRoute('complete')
+      })
+  }
+
+  const ErrorMsg = styled.div`
+    display: ${props => props.isVisible ? 'block' : 'none'};
+    color: red;
+  `
 
   return (
     <LACRMForm onSubmit={handleSubmit(onSubmit)}>
-      <select id="team-members" name="team-members" ref={register}>
-        <option value="">asdfasdfasdfasdf</option>
-      </select>
-      <textarea id="name" name="note" ref={register}></textarea>
-      <button type="submit" ref={register}>Submit</button>
+      <label htmlFor="churchMember">
+        Church Member: 
+        <select id="churchMember" name="churchMember" ref={register} required="true">
+          <option value="">Select a church member</option>
+          <option value="3726268736538631475369381625468">Alex Perez</option>
+        </select>
+      </label>
+      <label htmlFor="email">
+        Your Email: 
+        <input type="text" id="email" ref={register} name="email" required="true"/>
+      </label>
+      <label htmlFor="note">
+        Note:
+        <textarea id="name" name="note" ref={register} required="true"></textarea>
+      </label>
+      <button type="submit" name="submit" ref={register}>Submit</button>
+      <ErrorMsg isVisible={hasError}>There was an error submitting your note! Please check that your email is correct and try again</ErrorMsg>
     </LACRMForm>
   )
 }
 
-const Login = () => {
-  const uiConfig = {
-    signInFlow: 'popup',
-    signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ]
+const Complete = ({setRoute}) => {
+  const handleCreateAnother = () => {
+    setRoute('form')
   }
 
   return (
-    <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    <div>
+      Your message was sent.
+      <br />
+      <button onClick={handleCreateAnother}>Click here to create another note</button>
+    </div>
   )
 }
-
-const Complete = () => {}
 
 export const App = () => {
   const [route, setRoute] = useState('form')
 
-
   const routes = {
-    form: <NoteForm  setRoute={setRoute} />,
-    login: <Login/>,
-    complete: <Complete />,
+    form: <NoteForm setRoute={setRoute} />,
+    complete: <Complete  setRoute={setRoute} />,
   }
 
   return routes[route]
