b888403b8cb5ba447eb986a8258fc6df0ae5d283
Alex Perez <alexanderlperez@gmail.com>
2021-04-21 21:31:16 -0600

    Notes with all kinds of characters send correctly

--- functions/index.js
+++ functions/index.js
@@ -13,29 +13,24 @@ const API_URL = 'https://api.lessannoyingcrm.com/'
 
 // TODO: put these values in a .env file and make them switchable at deploy time
 
-// Production Test Data
+// Production 
 const USER_CODE = '95E24'
 const API_KEY = '613924-AgfdG3fCxn2wJpsHJNIbmQqMGBMkKEp2OYZSAISsRG3zvxwj0X' 
-
-// Local test data
-// const USER_CODE = 'A0DC5'
-// const API_KEY = '658885-AcIrvp5jbzw7qhfGvWGzfbZqEPZCmkHzUrc5G2fBOwVXnrJMSE' 
-// const DUMMY_CONTACT_ID = '3721904669969614100048897386911' // Test Contact
 
 const callAPI = async (func, params) => {
   let body = {
     UserCode: USER_CODE,
     APIToken: API_KEY,
     Function: func,
+    Parameters: JSON.stringify(params)
   }
 
-  const out = API_URL + '?' + new URLSearchParams(body).toString() + '&Parameters=' + JSON.stringify(params)
+  console.log('sending data\n', body)
 
-  console.log('sending data')
-  console.log({out})
-  return superagent
-    .post(out)
-    .catch(e => console.log(e))
+  return superagent.post(API_URL)
+    .type('form')
+    .send(body)
+    .catch(e => console.log('got error:', e))
     .then(res => res.body)
 }
 
@@ -43,17 +38,12 @@ app.use(cors);
 app.use(bodyParser.json());
 
 app.get('/createNote', async (req, res) => {
-  console.log('got data', req.query)
-
   const { note, churchMember, teamMember } = req.query
-
-  console.log({query: req.query})
-
   const out = await callAPI('CreateNote', {
     ContactId: churchMember,
     Note: 
-    `From ${encodeURIComponent(teamMember)} on ${format(utcToZonedTime(new Date(), 'America/New_York'), 'L/d/yyyy h:mmaaa')}:
-${encodeURIComponent(note)}
+    `From ${teamMember} on ${format(utcToZonedTime(new Date(), 'America/New_York'), 'L/d/yyyy h:mmaaa')}:
+${note}
 `
   })
 
